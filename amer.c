8765b80c669c2de5f6778ccf70dcf64d6005fccb
Amer Haddadeh <amer.haddadeh@gmail.com>
2018-10-16 10:36:53 +0300

    one more fact

--- amer.c
+++ amer.c
@@ -5,8 +5,19 @@ void my_vunction()
 	print("ammorti is more than stupid");
 }
 
+
+void func()
+{
+
+
+	print("you're the same !");
+
+}
+
+
 void main() 
 {
 	printf("ammoorti");
 	my_vunction();
+	func();
 }
